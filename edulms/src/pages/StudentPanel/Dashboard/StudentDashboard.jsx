1b0eeb7ffb42cd52c39177386188ba65b3540182
bidururkarthik <bidururkarthik1212@gmail.com>
2025-06-21 17:32:36 +0530

    update layout$

--- edulms/src/pages/StudentPanel/Dashboard/StudentDashboard.jsx
+++ edulms/src/pages/StudentPanel/Dashboard/StudentDashboard.jsx
@@ -363,11 +363,11 @@ const StudentDashboard1 = () => {
                                 <i className="bx bx-comment-dots align-middle text-muted me-1"></i>
                                 Duration: {course.duration} weeks
                               </a>
-                            </li>
+                            </li> 
                           </ul>
                           <p className="text-muted">
                             Level: {course.skillLevels}
-                          </p>
+                          </p> 
                           <p className="text-muted">₹ {course.price}</p>
 
                           <div>
